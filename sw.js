a74b4a3007c0696fbd33e3281a37ae49f2f7879c
PtitDoritos <ptit.doritos@gmail.com>
2026-08-13 10:07:09 +0200

    Update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Version du cache (passée en v2 pour forcer la mise à jour)
-const CACHE_NAME = 'ptitdoritos-v3';
+const CACHE_NAME = 'ptitdoritos-v3.0.1';
 
 // Liste des fichiers à mettre en mémoire hors-ligne
 const ASSETS = [
